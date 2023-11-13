57bd68820058277a9255bb748ff1900399ce8a89
fducau <fnd212@nyu.edu>
2023-11-13 11:15:04 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,178 @@
+import mediapipe as mp
+
+import streamlit as st
+import cv2
+import numpy as np
+import requests
+
+from process import (
+    draw_landmarks_on_image,
+    pose_estimation,
+    load_image_data,
+    estimate_abduction,
+    estimate_flexion,
+    get_coordinates,
+)
+
+
+cfg = {
+    "model_path": "./pretrained_models/pose_landmarker_heavy.task",
+    "post_url": "https://cms.horusleg.com/api/collections/CV_Angles_Felipe_Ducau/records",
+}
+
+
+def main():
+    st.session_state.loaded_landmarks = False
+    st.session_state.computed_angles = False
+    st.session_state.flexion_angle = None
+    st.session_state.abduction_angle = None
+
+    sample_image_front_path = "./sample_data/sample_front.jpg"
+    sample_image_side_path = "./sample_data/sample_side.jpg"
+
+    st.title("Prosthetics Measuring App")
+
+    # Using st.markdown
+    st.markdown("## Instructions")
+    # Create an expander
+    with st.expander("(1) Picture set up"):
+        st.markdown(
+            """
+            - Choose a place with solid background and uniform illumination.
+            - Use as little clothing as possible when taking the picture.
+            - The picture frame should capture the entire body shape.
+        """
+        )
+    with st.expander("(2) Front picture instructions"):
+        sample_image_front = cv2.imread(sample_image_front_path)
+
+        st.markdown(
+            """
+            - Place two identical chairs in front of the background.
+            - Hold both chairs with your hands from the upper part (one on each side of your body)
+            - Lift yourself with your arms suficiently so your standing leg is touching the floor but it is not sustaining your body weight.
+        """
+        )
+        st.image(
+            cv2.cvtColor(sample_image_front, cv2.COLOR_BGR2RGB),
+            caption="Sample front image",
+            width=200,
+        )
+
+    with st.expander("(3) Side picture instructions"):
+        sample_image_side = cv2.imread(sample_image_side_path)
+
+        st.markdown(
+            """
+            - Stand against the wall facing to the side in a way that your limp is facing the camera.
+            - Maintain your position by placing your hand against the background.
+                    """
+        )
+        st.image(
+            cv2.cvtColor(sample_image_side, cv2.COLOR_BGR2RGB),
+            caption="Sample front image",
+            width=200,
+        )
+
+    # Using st.markdown
+    st.markdown("## Analysis setup")
+
+    analysis_options = ["Abduction (front picture)", "Flexion (side picture)"]
+
+    analysis = st.selectbox("Type of Analysis:", analysis_options, index=None)
+
+    limp_leg = st.selectbox("Limp leg", ["RIGHT", "LEFT"], index=None)
+
+    if analysis is not None and limp_leg is not None:
+        uploaded_file = st.file_uploader(
+            "Choose an image...", type=["jpg", "jpeg", "png"]
+        )
+
+        if uploaded_file is not None:
+            test_number = uploaded_file.name[2:]
+            test_number = test_number.split(".")[0]
+            test_number = int(test_number)
+
+            st.image(uploaded_file, caption="Original Image", width=150)
+
+            mp_image = load_image_data(uploaded_file)
+
+            pose_detection = pose_estimation(mp_image, cfg)
+            annotated_image = draw_landmarks_on_image(
+                mp_image.numpy_view()[:, :, :3], pose_detection
+            )
+
+            st.image(
+                annotated_image,
+                caption="Processed Image",
+                use_column_width=True,
+                channels="GRAY",
+            )
+
+            if len(pose_detection.pose_landmarks) == 0:
+                st.error(
+                    """
+                    We couldn't estimate the pose from the
+                    picture you uploaded.
+                    Please try again with a new picture.
+                """
+                )
+
+            else:
+                analysis_landmarks = pose_detection.pose_landmarks[0][23:27]
+                st.success(
+                    """
+                    Pose successfully found.
+                    Please review image annotations.
+                    If the hips and knees are not properly
+                    detected, please try again with a new picture.
+                """
+                )
+                st.session_state.loaded_landmarks = True
+
+    if st.session_state.loaded_landmarks:
+        # Front analysis
+        if analysis == analysis_options[0]:
+            if st.button("Estimate Abduction Angle"):
+                analysis_result = estimate_abduction(analysis_landmarks, limp_leg)
+                st.write(f"{analysis_result:.2f} degrees")
+                flexion_angle = None
+                abduction_angle = analysis_result
+                st.session_state.computed_angles = True
+                st.session_state.abduction_angle = abduction_angle
+                st.session_state.flexion_angle = None
+
+        elif analysis == analysis_options[1]:
+            if st.button("Estimate Flexion Angle"):
+                analysis_result = estimate_flexion(analysis_landmarks, limp_leg)
+                st.write(f"{analysis_result:.2f} degrees")
+                flexion_angle = analysis_result
+                st.session_state.computed_angles = True
+                st.session_state.abduction_angle = None
+                st.session_state.flexion_angle = flexion_angle
+
+        # Post to Horus API
+        if st.session_state.computed_angles:
+            st.markdown("Analysis JSON")
+            post_data = {
+                "abduction_angle": st.session_state.abduction_angle,
+                "flexion_angle": st.session_state.flexion_angle,
+                "any_data_as_file": {
+                    "annotated_image": "cropped",
+                    "analysis_landmarks": get_coordinates(analysis_landmarks),
+                },
+                "test_number": test_number,
+            }
+            st.write(post_data)
+
+            if st.button("Post to API"):
+                import json
+
+                post_data["any_data_as_file"] = annotated_image.tolist()
+                response = requests.post(cfg["post_url"], json=json.dumps(post_data))
+                st.write(response)
+                print(response)
+
+
+if __name__ == "__main__":
+    main()
---
+++ process.py
@@ -0,0 +1,199 @@
+import matplotlib.pyplot as plt
+
+import mediapipe as mp
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+from geometer import Line, Point, angle
+import cv2
+import numpy as np
+
+import os
+import copy
+
+import math
+
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+def pose_estimation(rgb_image, cfg):
+    # Create Detector
+    base_options = python.BaseOptions(model_asset_path=cfg["model_path"])
+    options = vision.PoseLandmarkerOptions(
+        base_options=base_options, output_segmentation_masks=True
+    )
+    detector = vision.PoseLandmarker.create_from_options(options)
+
+    # detect
+    detection_result = detector.detect(rgb_image)
+    return detection_result
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    """
+    Overlay detection results on top of RGB image.
+    Highlights hips and knees.
+
+    :param rgb_image: <np.array> input RGB image
+    :detection_result: <PoseLandmarkerResult> as outcome from
+                       vision.PoseLandmarker detector.
+    :returns: annotated image
+    """
+    pose_landmarks_list = detection_result.pose_landmarks
+    annotated_image = np.copy(rgb_image)
+
+    # Loop through the detected poses to visualize.
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks = pose_landmarks_list[idx]
+
+        # Draw the pose landmarks.
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend(
+            [
+                landmark_pb2.NormalizedLandmark(
+                    x=landmark.x, y=landmark.y, z=landmark.z
+                )
+                for landmark in pose_landmarks
+            ]
+        )
+        styles = solutions.drawing_styles.get_default_pose_landmarks_style()
+        for k in styles.keys():
+            if k.real in [26, 25, 24, 23]:
+                st = copy.deepcopy(styles[k])
+                st.thickness = 15
+                st.color = (210, 97, 255)
+                styles[k] = st
+
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            pose_landmarks_proto,
+            solutions.pose.POSE_CONNECTIONS,
+            styles,
+        )
+    return annotated_image
+
+
+def cv2_imshow(img):
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(image)
+    plt.show()
+
+
+def load_image_data(uploaded_file):
+    image_data = uploaded_file.read()
+
+    # Convert the bytes to a NumPy array
+    np_image = np.frombuffer(image_data, dtype=np.uint8)
+
+    # Use OpenCV to decode the NumPy array into an image
+    # Use 1 to load color image, 0 for grayscale
+    opencv_image = cv2.imdecode(np_image, 1)
+    mp_image = mp.Image(
+        data=cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB),
+        image_format=mp.ImageFormat.SRGB,
+    )
+    return mp_image
+
+
+def get_coordinates(pose_landmarks):
+    """
+    Get x, y coordinates from list of
+    NormalizedLandmarks
+    """
+    coordinates = []
+    for pl in pose_landmarks:
+        coordinates.append((pl.x, pl.y))
+    return coordinates
+
+
+def center_vector(vector):
+    rv = vector
+    rv[:, 0] -= rv[0, 0]
+    rv[:, 1] -= rv[0, 1]
+
+    return rv
+
+
+def find_perpendicular_vector(point1, point2):
+    x0, y0 = point1
+    x1, y1 = point2
+    perpendicular_vector = (y1 - y0, -(x1 - x0))
+    return perpendicular_vector
+
+
+def estimate_abduction(pose_landmarks, limp_leg):
+    """
+    :param pose_landmarks: <list>
+        List of NormalizedLandmark objects.
+        corresponding to:
+        [left_hip, right_hip, left_knee, right_knee]
+
+    :param limp_leg: <str> 'RIGHT'|'LEFT'
+    :returns: <float> abduction angle in degrees.
+    """
+    pose_coordinates = get_coordinates(pose_landmarks)
+    pose_points = [Point(*pc) for pc in pose_coordinates]
+    hip_vector = Line(pose_points[0], pose_points[1])
+
+    # Compute the vertical reference
+    # perpendicular to hip line
+    if limp_leg == "LEFT":
+        leg_vector = Line(pose_points[0], pose_points[2])
+        vertical_reference = hip_vector.perpendicular(through=pose_points[0])
+    else:
+        leg_vector = Line(pose_points[1], pose_points[3])
+        vertical_reference = hip_vector.perpendicular(through=pose_points[1])
+
+    # Make sure that the vertical reference is pointing down
+    if vertical_reference.direction[1] < 0:
+        vertical_reference = Line(-vertical_reference)
+
+    # Compute abduction angle
+    if limp_leg == "LEFT":
+        abduction_dgr = angle(vertical_reference, leg_vector) * 180 / math.pi
+    else:
+        abduction_dgr = -angle(vertical_reference, leg_vector) * 180 / math.pi
+
+    return abduction_dgr
+
+
+def estimate_flexion(pose_landmarks, limp_leg):
+    """
+    :param pose_landmarks: <list>
+        List of NormalizedLandmark objects.
+        corresponding to:
+        [left_hip, right_hip, left_knee, right_knee]
+
+    :param limp_leg: <str> 'RIGHT'|'LEFT'
+    :returns: <float> flexion angle in degrees.
+    """
+    pose_coordinates = get_coordinates(pose_landmarks)
+    pose_points = [Point(*pc) for pc in pose_coordinates]
+
+    vertical_line = Line(Point(0, 0), Point(0, 1))
+
+    # Compute the vertical reference
+    # perpendicular to hip line
+    if limp_leg == "LEFT":
+        leg_vector = Line(pose_points[0], pose_points[2])
+        vertical_reference = vertical_line.parallel(through=pose_points[0])
+    else:
+        leg_vector = Line(pose_points[1], pose_points[3])
+        vertical_reference = vertical_line.parallel(through=pose_points[1])
+
+    # Make sure that the vertical reference is pointing down
+    if vertical_reference.direction[1] < 0:
+        vertical_reference = Line(-vertical_reference)
+
+    # Compute abduction angle
+    if limp_leg == "LEFT":
+        flexion_dgr = angle(leg_vector, vertical_reference) * 180 / math.pi
+    else:
+        flexion_dgr = angle(vertical_reference, leg_vector) * 180 / math.pi
+
+    return flexion_dgr
